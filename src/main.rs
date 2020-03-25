707a884ba8eb569852ce48f81bf885cab2d22386
Kevin Simper <kevin.simper@gmail.com>
2020-03-25 03:23:46 +0100

    Add a basic test for index route

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use actix_web::{get, App, HttpServer, Responder};
+use futures::executor::block_on;
+use actix_web::{web, test, App, HttpServer, Responder};
 
-#[get("/")]
 async fn index() -> impl Responder {
     format!("Hello from Rust")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(web::resource("/").to(index)))
         .bind("127.0.0.1:8000")?
         .run()
         .await
+}
+
+#[actix_rt::test]
+async fn test_index() {
+    let srv = test::start(
+        || App::new().service(
+                web::resource("/").to(index))
+    );
+
+    let req = srv.get("/");
+    let mut response = req.send().await.unwrap();
+    let rawbody = block_on(response.body()).unwrap();
+    let message = String::from_utf8(rawbody.to_vec()).unwrap();
+    println!("{}", message);
+    assert!(message == String::from("Hello from Rust"));
+    assert!(response.status().is_success());
 }
